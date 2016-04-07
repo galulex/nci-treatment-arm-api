dfbe8e36c3cd816ebdc3283320e90c8f8366d097
imativo <isaac.mativo@nih.gov>
2016-04-07 12:12:35 -0400

    Added field types for basic_treatment_arm model

--- app/models/basic_treatment_arm.rb
+++ app/models/basic_treatment_arm.rb
@@ -6,16 +6,16 @@ class BasicTreatmentArm
   store_in collection: "basic_treatment_arm"
 
   field :_id, type: String, default: ->{ _id }
-  field :treatmentArmName
-  field :currentPatients
-  field :formerPatients
-  field :notEnrolledPatients
-  field :pendingPatients
+  field :treatmentArmName, type: String
+  field :currentPatients, type: Integer
+  field :formerPatients, type: Integer
+  field :notEnrolledPatients, type: Integer
+  field :pendingPatients, type: Integer
   field :treatmentArmStatus
-  field :dateCreated
-  field :dateOpened
-  field :dateClosed
-  field :dateSuspended
+  field :dateCreated, type: DateTime, default: Time.now
+  field :dateOpened, type: DateTime
+  field :dateClosed, type: DateTime
+  field :dateSuspended, type: DateTime
 
 end
 
