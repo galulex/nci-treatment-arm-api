8024721d3e11ffe75f938f32aa35c2f51e14b672
imativo <isaac.mativo@nih.gov>
2016-04-25 12:06:42 -0400

    Added spec file for non_hotspot_rule and user

--- spec/models/treatment_arm/non_hotspot_rule_spec.rb
+++ spec/models/treatment_arm/non_hotspot_rule_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-# describe NonHotspotRule do
-#
-#   it { is_expected.to have_fields(:description, :oncomine_variant_class, :gene_name, :exon, :function, :level_Of_Evidence,
-#                       :literature_reference, :special_rules, :protein_match).of_type(Object)}
-#
-# end
+describe NonHotspotRule do
+
+  it { is_expected.to have_fields(:description, :oncomine_variant_class, :gene_name, :exon, :function, :level_of_evidence,
+                      :literature_reference, :special_rules, :protein_match).of_type(Object)}
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe User do
+
+  it "should test the User model"
+
+end
