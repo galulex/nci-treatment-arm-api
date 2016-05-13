e2585f08d71e8dd761b78e84ec2c68e577bac270
josephverbeck <joseph.verbeck@nih.gov>
2016-05-13 15:09:27 -0400

    Fixing secret loads for keys and such

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,7 @@
+
 Aws.config.update({
                       endpoint: ENV['aws_dynamo_endpoint'],
-                      access_key_id: ENV['aws_access_key_id'],
-                      secret_access_key: ENV['aws_secret_access_key'],
+                      access_key_id: Rails.application.secrets.aws_access_key_id,
+                      secret_access_key: Rails.application.secrets.aws_secret_access_key,
                       region: ENV['aws_region']
                   })
