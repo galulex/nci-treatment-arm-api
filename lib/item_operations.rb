97bbb77163ab93836e3c09f3262dfceedeb22514
josephverbeck <joseph.verbeck@nih.gov>
2016-05-17 13:11:03 -0400

    Adding in prefix to yml file

--- lib/item_operations.rb
+++ lib/item_operations.rb
@@ -2,21 +2,16 @@
 module Aws
   module Record
 
-
-    def create_table
-      if (!self.table_exists?)
-        migration = Aws::Record::TableMigration.new(self)
-        migration.create!(provisioned_throughput: { read_capacity_units: 5, write_capacity_units: 5 })
-        migration.wait_until_available
-      end
-    end
-
     module ItemOperations
       module ItemOperationsClassMethods
         alias_method :original_find, :find
 
         def find(opts)
-          create_table
+          if (!self.table_exists?)
+            migration = Aws::Record::TableMigration.new(self)
+            migration.create!(provisioned_throughput: { read_capacity_units: 5, write_capacity_units: 5 })
+            migration.wait_until_available
+          end
           original_find(opts)
         end
 
@@ -28,7 +23,11 @@ module Aws
         alias_method :orginal_scan, :scan
 
         def scan(opts = {})
-          create_table
+          if (!self.table_exists?)
+            migration = Aws::Record::TableMigration.new(self)
+            migration.create!(provisioned_throughput: { read_capacity_units: 5, write_capacity_units: 5 })
+            migration.wait_until_available
+          end
           orginal_scan(opts)
         end
       end
