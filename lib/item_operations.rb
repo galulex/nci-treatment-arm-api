681d432aa9e28981da2d7581596328b027c20f39
josephverbeck <joseph.verbeck@nih.gov>
2016-05-17 12:21:32 -0400

    Adding in code that create tables if table doesnt exist on scan and find

--- lib/item_operations.rb
+++ lib/item_operations.rb
@@ -1,19 +1,36 @@
 
 module Aws
   module Record
+
+
+    def create_table
+      if (!self.table_exists?)
+        migration = Aws::Record::TableMigration.new(self)
+        migration.create!(provisioned_throughput: { read_capacity_units: 5, write_capacity_units: 5 })
+        migration.wait_until_available
+      end
+    end
+
     module ItemOperations
       module ItemOperationsClassMethods
         alias_method :original_find, :find
 
         def find(opts)
-          if (!self.table_exists?)
-                migration = Aws::Record::TableMigration.new(self)
-                migration.create!(provisioned_throughput: { read_capacity_units: 5, write_capacity_units: 5 })
-                migration.wait_until_available
-          end
+          create_table
           original_find(opts)
         end
 
+      end
+    end
+
+    module Query
+      module QueryClassMethods
+        alias_method :orginal_scan, :scan
+
+        def scan(opts = {})
+          create_table
+          orginal_scan(opts)
+        end
       end
     end
   end
