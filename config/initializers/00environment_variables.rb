45c0c1201bf7fda08abc228b76b5cb94109222eb
josephverbeck <joseph.verbeck@nih.gov>
2016-05-20 15:30:56 -0400

    Fixing env load

--- config/initializers/00environment_variables.rb
+++ config/initializers/00environment_variables.rb
@@ -3,9 +3,9 @@
 module EnvironmentVariables
   class Application < Rails::Application
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'environment.yml')#"#{File.expand_path('~')}/local/content/ncimatch/conf/ruby/auth0-prodtest.yml"
+      env_file = File.join(Rails.root, 'config', 'environment.yml')
       if File.exists?(env_file)
-        YAML.load_file(env_file)[::Rails.env].each {|k,v| ENV[k] = v }
+        YAML.load_file(env_file)[Rails.env].each {|k,v| ENV[k] = v }
       end
     end
   end
