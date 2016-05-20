fcf9fb17ea1cf15da465a7ad9b68f4b69642f7a3
futo82 <tony.fu@nih.gov>
2016-05-20 16:44:53 -0400

    created a shell script to kick off the bddtests command

---
+++ trigger_bddtests_build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d "{ 'request': { 'branch' : 'master' } }" https://api.travis-ci.org/repositories/CBIIT/nci-uMatch-bddtests
