23d3009a290c666e665d4eeb86e9511956820949
josephverbeck <joseph.verbeck@nih.gov>
2016-05-20 16:00:19 -0400

    Working on env variables still

--- config/initializers/00environment_variables.rb
+++ config/initializers/00environment_variables.rb
@@ -5,7 +5,9 @@ module EnvironmentVariables
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'environment.yml')
       if File.exists?(env_file)
-        YAML.load_file(env_file)[Rails.env].each {|k,v| ENV[k] = v }
+        YAML.load_file(env_file)[Rails.env].each do |key, value|
+          ENV[key.to_s] = value
+        end
       end
     end
   end
