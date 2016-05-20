cea0516c7330ea1369fa666db987e8117b6d0e54
josephverbeck <joseph.verbeck@nih.gov>
2016-05-20 15:20:29 -0400

    Following travis ci instructions and add eager loading

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   # making false as eager_load doesn't play well with Knock -jv
-  config.eager_load = false
+  config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
