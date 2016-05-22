a7fcf20f825d8287e4ee90b23a573831d7c8814c
josephverbeck <joseph.verbeck@nih.gov>
2016-05-22 16:18:49 -0400

    Fixing return status

--- app/controllers/treatmentarm_controller.rb
+++ app/controllers/treatmentarm_controller.rb
@@ -10,7 +10,7 @@ class TreatmentarmController < ApplicationController
       treatment_arm_model = TreatmentArm.new.from_json(TreatmentArm.new.convert_models(@treatment_arm).to_json)
       if treatment_arm_model.valid?
         Aws::Publisher.publish(@treatment_arm)
-        render json: {:status => "SUCCESS"}, :status => 200
+        render json: {:status => "Success"}, :status => 200
       else
         render json: {:status => "FAILURE", :message => "Validation failed.  Please check all required fields are present"}, :status => 400
       end
