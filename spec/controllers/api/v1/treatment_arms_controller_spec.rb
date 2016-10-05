a7d739a8459518e3d5f1748ced04000421949879
asampatoor <adithya.sampatoor@nih.gov>
2016-10-05 19:18:41 -0400

    Fixed a Test case

--- spec/controllers/api/v1/treatment_arms_controller_spec.rb
+++ spec/controllers/api/v1/treatment_arms_controller_spec.rb
@@ -128,14 +128,7 @@ describe Api::V1::TreatmentArmsController do
         params = { id: 'EAY131-A', stratum_id: '100', version: '2017-10-07' }
         post :assignment_event, params.to_json, params.merge(format: 'json')
         expect(response).to_not be_nil
-      end
-    end
-
-    context 'With Invalid Data / Invalid Route' do
-      it 'Should not save data to the DataBase' do
-        params = { id: 'EAY131-A', stratum_id: '100', version: 2017-10-07 }
-        post :assignment_event, params.to_json, params.merge(format: 'json')
-        expect(response).to have_http_status(500)
+        expect(response).to have_http_status(200)
       end
     end
   end
