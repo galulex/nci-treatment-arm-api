b262e7f8df420570e125621def9521d986f37a82
asampatoor <adithya.sampatoor@nih.gov>
2016-10-25 11:19:14 -0400

    Handled some exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  rescue_from Aws::DynamoDB::Errors::ResourceNotFoundException, with: :no_resource_found_exception
+  rescue_from Aws::DynamoDB::Errors::ResourceNotFoundException, with: :resource_not_found_exception
+  rescue_from Seahorse::Client::NetworkingError, with: :no_db_connection_exception
 
   protected
 
-  def no_resource_found_exception
-    render json: { message: "TreatmentArm table doesn't exist, Please create the table by restarting the TreatmentArmProcessorAPI server" }, status: 400
+  def resource_not_found_exceptions
+    render json: { message: "One of the Tables doesn't exist, Please create the table by restarting the TreatmentArmProcessorAPI server" }, status: 400
+  end
+
+  def no_db_connection_exception
+    render json: { message: "Database is not ACTIVE" }, status: 502
   end
 end
