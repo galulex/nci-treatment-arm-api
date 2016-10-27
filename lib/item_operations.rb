56c9957f5f2b364e5abd3cf401e891e7d6339208
asampatoor <adithya.sampatoor@nih.gov>
2016-10-27 15:37:29 -0400

    Fixing a broken build Commit 2238d1e

--- lib/item_operations.rb
+++ lib/item_operations.rb
@@ -1,15 +1,14 @@
 
 module Aws
   module Record
-
     module ItemOperations
       module ItemOperationsClassMethods
         alias_method :original_find, :find
 
         def find(opts)
-          if (!self.table_exists?)
+          unless self.table_exists?
             migration = Aws::Record::TableMigration.new(self)
-            migration.create!(provisioned_throughput: { read_capacity_units: Rails.configuration.environment.fetch("read_capacity_units").to_i, write_capacity_units: Rails.configuration.environment.fetch("write_capacity_units").to_i })
+            migration.create!(provisioned_throughput: { read_capacity_units: ENV["read_capacity_units"].to_i, write_capacity_units: ENV["write_capacity_units"].to_i })
             migration.wait_until_available
           end
           original_find(opts)
@@ -23,9 +22,9 @@ module Aws
         alias_method :orginal_scan, :scan
 
         def scan(opts = {})
-          if (!self.table_exists?)
+          unless self.table_exists?
             migration = Aws::Record::TableMigration.new(self)
-            migration.create!(provisioned_throughput: { read_capacity_units: Rails.configuration.environment.fetch("read_capacity_units").to_i, write_capacity_units: Rails.configuration.environment.fetch("write_capacity_units").to_i })
+            migration.create!(provisioned_throughput: { read_capacity_units: ENV["read_capacity_units"].to_i, write_capacity_units: ENV["write_capacity_units"].to_i })
             migration.wait_until_available
           end
           orginal_scan(opts)
