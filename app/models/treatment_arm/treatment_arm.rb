c73a03672fed7cd07a0c608c53d3c4a20de43e36
asampatoor <adithya.sampatoor@nih.gov>
2016-11-01 09:52:56 -0400

    Refactored the PUT call again to get the latest TA Status

--- app/models/treatment_arm/treatment_arm.rb
+++ app/models/treatment_arm/treatment_arm.rb
@@ -98,23 +98,25 @@ class TreatmentArm
   def self.async_cog_status_update
     begin
       result = []
+      treatment_arms = TreatmentArm.scan({})
       response = HTTParty.get(Rails.configuration.environment.fetch('cog_url') + Rails.configuration.environment.fetch('cog_treatment_arms'))
-      cog_arms_status = response.parsed_response.deep_transform_keys!(&:underscore).symbolize_keys
-      cog_arms_status[:treatment_arms].each do |cog_arm|
-        match_treatment_arm = TreatmentArm.find_by(cog_arm['treatment_arm_id'], cog_arm['stratum_id'], nil, false).first
-        unless match_treatment_arm.blank?
-          next if match_treatment_arm.active == false
-          if match_treatment_arm.treatment_arm_status != 'CLOSED' && match_treatment_arm.treatment_arm_status != cog_arm['status']
-            Aws::Publisher.publish(cog_treatment_refresh: match_treatment_arm.attributes_data)
-            match_treatment_arm.treatment_arm_status = cog_arm['status']
-            result << match_treatment_arm.attributes_data
+      cog_arms = response.parsed_response.deep_transform_keys!(&:underscore).symbolize_keys
+      treatment_arms.each do |treatment_arm|
+        next if treatment_arm.active == false
+        cog_arms[:treatment_arms].each do |cog_arm|
+          if cog_arm['stratum_id'] == treatment_arm.stratum_id &&
+            cog_arm['treatment_arm_id'] == treatment_arm.treatment_arm_id &&
+            treatment_arm.treatment_arm_status != 'CLOSED' &&
+            treatment_arm.treatment_arm_status != cog_arm['status']
+            Aws::Publisher.publish(cog_treatment_refresh: treatment_arm.attributes_data)
+            treatment_arm.treatment_arm_status = cog_arm['status']
           end
         end
-       Rails.logger.info("No Treatment Arms status to update")
+        result << treatment_arm
       end
       result
     rescue => error
-      Rails.logger.warn("Failed to connect to COG with error #{error}::#{error.backtrace}")
+      raise "Failed connecting to COG :: #{error}"
     end
   end
 
