daa45da529af37e6b7d131400ca90d73e6722b7b
asampatoor <adithya.sampatoor@nih.gov>
2016-11-04 11:42:55 -0400

    changing the uat file to set the log_level lo INFO

--- config/environments/uat.rb
+++ config/environments/uat.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  #config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -74,7 +74,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   # config.log_formatter = ::Logger::Formatter.new
   # Set log level
-  config.log_level = :WARN
+  config.log_level = :INFO
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
   # config.log_tags = [ "Treatment-Arm-API", :subdomain, :uuid ]
