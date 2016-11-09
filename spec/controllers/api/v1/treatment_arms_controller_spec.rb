801f83327eab65bdc12ae140e4d6af74db98bb4c
asampatoor <adithya.sampatoor@nih.gov>
2016-11-09 15:15:27 -0500

    Moving Test Data to Factories to look cleaner

--- spec/controllers/api/v1/treatment_arms_controller_spec.rb
+++ spec/controllers/api/v1/treatment_arms_controller_spec.rb
@@ -6,55 +6,7 @@ describe Api::V1::TreatmentArmsController do
     setup_knock
   end
 
-  let(:treatment_arm) do
-    stub_model TreatmentArm,
-               treatment_arm_id: 'APEC1621-A',
-               version: '2016-20-02',
-               stratum_id: '12',
-               description: 'This is the sample Description',
-               target_id: 'HDFD',
-               target_name: 'OtherHen',
-               gene: 'GENE',
-               treatment_arm_drugs:
-               [
-                 {
-                   'drug_id': '113',
-                   'name': 'a'
-                 }
-               ],
-               snv_indels:
-                 [
-                   {
-                     'position': '30035190',
-                     'ocp_alternative': '-',
-                     'gene': 'NRAS',
-                     'protein': 'p.Q61H',
-                     'level_of_evidence': '3.0',
-                     'ocp_reference': 'TTC',
-                     'variant_type': 'snp',
-                     'chromosome': 'chr22',
-                     'arm_specific': false,
-                     'identifier': 'COSM22189',
-                     'public_med_ids':
-                     [
-                       '18827604',
-                       '21917678',
-                       '23181703'
-                     ],
-                     'inclusion': true
-                    }
-                  ],
-               diseases:
-                 [
-                   {
-                     'disease_code': '8200/0',
-                     'disease_code_type': 'ICD-O',
-                     'exclusion': false,
-                     'disease_name': 'Eccrine dermal cylindroma (C44._)\nTurban tumor (C44.4)\nCylindroma of skin (C44._)'
-                   }
-                 ],
-               exclusion_drugs: []
-  end
+  treatment_arm = FactoryGirl.build(:treatment_arm)
 
   describe 'POST #treatment_arm' do
     context 'with valid data' do
--- spec/support/factories/treatment_arm_factory.rb
+++ spec/support/factories/treatment_arm_factory.rb
@@ -1,12 +1,48 @@
 # require 'factory_girl'
 
 FactoryGirl.define do
-  factory :treatmentArm, :class => TreatmentArm do
-    name "EAY131-test"
+  factory :treatment_arm, :class => TreatmentArm do
+    name 'EAY131-test'
+    treatment_arm_id 'APEC1621-A'
+    version '2016-20-02'
+    stratum_id '12'
+    date_created '2020-10-05'
+    description 'This is the sample Description'
+    target_id 'HDFD'
+    target_name 'OtherHen'
+    gene 'GENE'
+    treatment_arm_drugs [{
+        'drug_id': '113',
+        'name': 'a'}]
+    snv_indels [
+        {
+          'position': '30035190',
+          'ocp_alternative': '-',
+          'gene': 'NRAS',
+          'protein': 'p.Q61H',
+          'level_of_evidence': '3.0',
+          'ocp_reference': 'TTC',
+          'variant_type': 'snp',
+          'chromosome': 'chr22',
+          'arm_specific': false,
+          'identifier': 'COSM22189',
+          'public_med_ids': ['18827604', '21917678', '23181703'],
+          'inclusion': true
+         }
+       ]
+    diseases [
+        {
+          'disease_code': '8200/0',
+          'disease_code_type': 'ICD-O',
+          'exclusion': false,
+          'disease_name': 'Eccrine dermal cylindroma (C44._)\nTurban tumor (C44.4)\nCylindroma of skin (C44._)'
+        }
+      ]
+    exclusion_drugs []
   end
 
   factory :treatmentArmVersions, :class => TreatmentArm do
-    name "EAY131-test-Version"
+    name 'EAY131-test-Version'
     version Time.now
   end
 end
