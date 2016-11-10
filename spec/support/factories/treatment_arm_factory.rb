fc64508c3c3798ce10b47a0d80a4b535e7924b65
asampatoor <adithya.sampatoor@nih.gov>
2016-11-10 12:40:57 -0500

    Refactored the Specs to be more readable

--- spec/support/factories/treatment_arm_factory.rb
+++ spec/support/factories/treatment_arm_factory.rb
@@ -6,9 +6,14 @@ FactoryGirl.define do
     treatment_arm_id 'APEC1621-A'
     version '2016-20-02'
     stratum_id '12'
+    active true
     date_created '2020-10-05'
     description 'This is the sample Description'
     target_id 'HDFD'
+    treatment_arm_status 'OPEN'
+    num_patients_assigned 2
+    date_opened '2015-10-07'
+    study_id 'APEC1621'
     target_name 'OtherHen'
     gene 'GENE'
     treatment_arm_drugs [{
@@ -39,10 +44,39 @@ FactoryGirl.define do
         }
       ]
     exclusion_drugs []
+    assay_rules []
+    gene_fusions []
+    non_hotspot_rules []
+    copy_number_variants []
+    not_enrolled_patients 1
+    former_patients 1
+    pending_patients 1
+    current_patients 1
+    version_pending_patients 1
+    version_former_patients 2
+    version_not_enrolled_patients 1
+    version_current_patients 1
   end
 
-  factory :treatmentArmVersions, :class => TreatmentArm do
-    name 'EAY131-test-Version'
-    version Time.now
+  factory :treatment_arm_assignment_event, :class => TreatmentArmAssignmentEvent do
+    assignment_date '2012-02-20'
+    date_on_arm '2016-05-27'
+    date_off_arm '2013-01-19'
+    patient_id '200re'
+    treatment_arm_id 'EAC123'
+    stratum_id 'EAY131'
+    patient_status 'PENDING_CONFIRMATION'
+    assignment_reason ''
+    diseases [
+      { 'drug_id': '1234' }
+    ]
+    version '2012-02-20'
+    step_number '0'
+    analysis_id '1'
+    molecular_id '2'
+    surgical_event_id '3'
+    variant_report {}
+    assignment_report {}
+    event 'EVENT_INIT'
   end
 end
