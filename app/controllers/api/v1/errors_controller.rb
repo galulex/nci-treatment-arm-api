0b72c91d500209705b22685b6d28a83e4d47a909
asampatoor <adithya.sampatoor@nih.gov>
2016-11-21 13:57:48 -0500

    Adding Ping controller spec

--- app/controllers/api/v1/errors_controller.rb
+++ app/controllers/api/v1/errors_controller.rb
@@ -4,7 +4,7 @@ module Api
 
       def render_not_found
         respond_to do |format|
-          format.json { render json: { message: "No Route matches /api/v1/#{params[:path]} " }, layout: false, status: :bad_request }
+          format.json { render json: { message: "No Route matches /api/v1/#{params[:path]}" }, layout: false, status: :bad_request }
           format.any { head :not_acceptable }
         end
       end
