91ca9f0d3fd4e7bea125be4eac214ddd64839a2e
asampatoor <adithya.sampatoor@nih.gov>
2016-11-29 14:58:43 -0500

    Changed routing error to show bad request instead of not acceptable

--- app/controllers/api/v1/errors_controller.rb
+++ app/controllers/api/v1/errors_controller.rb
@@ -5,7 +5,7 @@ module Api
       def render_not_found
         respond_to do |format|
           format.json { render json: { message: "No Route matches /api/v1/#{params[:path]}" }, layout: false, status: :bad_request }
-          format.any { head :not_acceptable }
+          format.any { head :bad_request }
         end
       end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,16 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   rescue_from Aws::DynamoDB::Errors::ResourceNotFoundException, with: :resource_not_found_exception
   rescue_from Seahorse::Client::NetworkingError, with: :no_db_connection_exception
+  rescue_from ActionController::RoutingError, with: lambda { |exception| render_error(:bad_request, exception) }
 
   protected
+
+  def render_error(status, exception)
+    logger.error status.to_s +  " " + exception.to_s
+    respond_to do |format|
+      format.all { head status}
+    end
+  end
 
   def resource_not_found_exceptions
     render json: { message: "One of the Tables doesn't exist, Please create the table by restarting the TreatmentArmProcessorAPI server" }, status: 400
