5669fd819a1476cca0cd4152ae8eadc8e46d968c
asampatoor <adithya.sampatoor@nih.gov>
2016-11-29 14:41:12 -0500

    Fixed bug in sending the message onto the queue

--- app/controllers/api/v1/treatment_arms_controller.rb
+++ app/controllers/api/v1/treatment_arms_controller.rb
@@ -140,8 +140,8 @@ module Api
       def clone_params
         body_params = JSON.parse(request.raw_post)
         body_params.deep_transform_keys!(&:underscore).symbolize_keys!
-        body_params[:new_version] = params[:version]
-        [:treatment_arm_id, :date_created, :version, :stratum_id].each { |k| body_params.delete(k) }
+        body_params[:version] = params[:version]
+        [:treatment_arm_id, :date_created, :stratum_id].each { |k| body_params.delete(k) }
         body_params[:stratum_id] = params[:stratum_id]
         body_params
       end
