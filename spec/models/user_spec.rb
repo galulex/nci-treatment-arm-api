9e94526556d9d074bcd446f508e1cfc1708ab2e8
asampatoor <adithya.sampatoor@nih.gov>
2016-12-18 16:47:33 -0500

    User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,15 @@
 
-describe User, :type => :model do
+describe User, type: :model do
   it 'can create empty object' do
     expect(User.new).to_not be nil
   end
 
   it 'has function has_secure_password' do
     expect(User.respond_to?(:has_secure_password)).to eq(true)
+  end
+
+  it 'should not have empty payload' do
+    expect(User.from_token_payload 'payload').to be_truthy
+    expect(User.from_token_payload 'payload').to_not be_nil
   end
 end
