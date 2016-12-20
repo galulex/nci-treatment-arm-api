50285d751f931982995fd39092f6b6cfd76bda7a
asampatoor <adithya.sampatoor@nih.gov>
2016-12-20 10:15:17 -0500

    Removing error handling as this is no longer working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  rescue_from Aws::DynamoDB::Errors::ResourceNotFoundException, with: :resource_not_found_exception
   rescue_from Seahorse::Client::NetworkingError, with: :no_db_connection_exception
   rescue_from ActionController::RoutingError, with: lambda { |exception| render_error(:bad_request, exception) }
 
@@ -13,12 +12,8 @@ class ApplicationController < ActionController::Base
   def render_error(status, exception)
     logger.error status.to_s +  " " + exception.to_s
     respond_to do |format|
-      format.all { head status}
+      format.all { head status }
     end
-  end
-
-  def resource_not_found_exceptions
-    render json: { message: "One of the Tables doesn't exist, Please create the table by restarting the TreatmentArmProcessorAPI server" }, status: 400
   end
 
   def no_db_connection_exception
