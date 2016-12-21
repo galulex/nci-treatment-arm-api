838664883d8f00f3e0348e598309a3df389c2476
asampatoor <adithya.sampatoor@nih.gov>
2016-12-21 16:51:39 -0500

    Fix code indentation issues

--- spec/models/treatment_arm/treatment_arm_spec.rb
+++ spec/models/treatment_arm/treatment_arm_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe TreatmentArm do
-
   let(:api_requests) { [] }
 
   let(:stub_client) do
@@ -69,10 +68,10 @@ describe TreatmentArm do
   end
 
   it 'automatically declares treatment_arm_id' do
-    expect{treatment_arm.treatment_arm_id}.to_not raise_error
+    expect { treatment_arm.treatment_arm_id }.to_not raise_error
   end
 
-  it "should be valid when an instance is created" do
+  it 'should be valid when an instance is created' do
     expect(TreatmentArm.new).to be_truthy
   end
 
@@ -82,7 +81,7 @@ describe TreatmentArm do
   end
 
   it 'should be of the correct instance' do
-    treatment_arm = TreatmentArm.new()
+    treatment_arm = TreatmentArm.new
     expect(treatment_arm).to be_an_instance_of(TreatmentArm)
   end
 
@@ -101,7 +100,6 @@ describe TreatmentArm do
   end
 
   describe 'Attributes' do
-
     let(:record) do
       Class.new do
         include(Aws::Record)
@@ -109,7 +107,6 @@ describe TreatmentArm do
     end
 
     describe '#initialize' do
-
       let(:model) do
         Class.new do
           include(Aws::Record)
@@ -134,7 +131,6 @@ describe TreatmentArm do
     end
 
     describe 'Keys' do
-
       it 'should be able to assign a hash key' do
         record.string_attr(:treatment_arm_id, hash_key: true)
         expect(record.hash_key).to eq(:treatment_arm_id)
@@ -148,14 +144,11 @@ describe TreatmentArm do
       end
 
       it 'should reject assigning the same attribute as hash and range key' do
-        expect {
-          record.string_attr(:treatment_arm_id, hash_key: true, range_key: true)
-          }.to raise_error(ArgumentError)
+        expect { record.string_attr(:treatment_arm_id, hash_key: true, range_key: true) }.to raise_error(ArgumentError)
       end
     end
 
     describe 'Attributes' do
-
       it 'should create dynamic methods around attributes' do
         record.string_attr(:description)
         x = record.new
@@ -164,28 +157,22 @@ describe TreatmentArm do
       end
 
       it 'should reject non-symbolized attribute names' do
-        expect { record.integer_attr("integer") }.to raise_error(ArgumentError)
+        expect { record.integer_attr('integer') }.to raise_error(ArgumentError)
       end
 
       it 'rejects collisions of db storage names with existing attr names' do
         record.boolean_attr(:active, database_attribute_name: 'is_active_flag')
-        expect {
-          record.boolean_attr(:fail, database_attribute_name: 'active')
-          }.to raise_error(Aws::Record::Errors::NameCollision)
+        expect { record.boolean_attr(:fail, database_attribute_name: 'active') }.to raise_error(Aws::Record::Errors::NameCollision)
       end
 
       it 'rejects collisions of attr names with existing db storage names' do
         record.boolean_attr(:active, database_attribute_name: 'is_active_flag')
-        expect {
-          record.boolean_attr(:is_active_flag, database_attribute_name: 'fail')
-          }.to raise_error(Aws::Record::Errors::NameCollision)
+        expect { record.boolean_attr(:is_active_flag, database_attribute_name: 'fail') }.to raise_error(Aws::Record::Errors::NameCollision)
       end
 
       it 'should not allow duplicate assignment of the same attr name' do
         record.string_attr(:target_name)
-        expect { record.datetime_attr(:target_name) }.to raise_error(
-          Aws::Record::Errors::NameCollision
-          )
+        expect { record.datetime_attr(:target_name) }.to raise_error(Aws::Record::Errors::NameCollision)
       end
 
       it 'should typecast an integer attribute' do
@@ -199,7 +186,7 @@ describe TreatmentArm do
         record.string_attr(:status_log)
         x = record.new
         x.status_log = '{"1479236708": "OPEN"}'
-        expect(x.to_h).to eq({ status_log: '{"1479236708": "OPEN"}' })
+        expect(x.to_h).to eq(status_log: '{"1479236708": "OPEN"}')
       end
 
       it 'should allow specification of a separate storage attribute name' do
@@ -211,30 +198,24 @@ describe TreatmentArm do
 
       it 'should reject storage name collisions' do
         record.string_attr(:active, database_attribute_name: 'is_active_flag')
-        expect {
-          record.string_attr(:is_active_flag)
-          }.to raise_error(Aws::Record::Errors::NameCollision)
-          expect(record.attributes.present?(:is_active_flag)).to be_falsy
+        expect { record.string_attr(:is_active_flag) }.to raise_error(Aws::Record::Errors::NameCollision)
+        expect(record.attributes.present?(:is_active_flag)).to be_falsy
       end
 
       it 'should enforce uniqueness of storage names' do
         record.string_attr(:active, database_attribute_name: 'is_active_flag')
-        expect {
-          record.string_attr(:treatment_arm_id, database_attribute_name: 'is_active_flag')
-          }.to raise_error(Aws::Record::Errors::NameCollision)
+        expect { record.string_attr(:treatment_arm_id, database_attribute_name: 'is_active_flag') }.to raise_error(Aws::Record::Errors::NameCollision)
       end
 
       it 'should not allow collisions with reserved names' do
-        expect {
-          record.string_attr(:to_h)
-          }.to raise_error(Aws::Record::Errors::ReservedName)
+        expect { record.string_attr(:to_h) }.to raise_error(Aws::Record::Errors::ReservedName)
       end
 
       it 'should allow reserved names to be used as custom storage names' do
         record.string_attr(:active, database_attribute_name: 'is_active_flag')
         item = record.new
         item.active = true
-        expect(item.to_h).to eq({ active: true })
+        expect(item.to_h).to eq(active: true)
       end
     end
   end
