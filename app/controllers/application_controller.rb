25c114026d17ce0087730fdc64a549ff658e2cf0
asampatoor <adithya.sampatoor@nih.gov>
2016-12-23 13:34:54 -0500

    Commenting out forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   rescue_from Aws::DynamoDB::Errors::ValidationException, with: :resource_not_found_exception
   rescue_from Seahorse::Client::NetworkingError, with: :no_db_connection_exception
   rescue_from ActionController::RoutingError, with: lambda { |exception| render_error(:bad_request, exception) }
