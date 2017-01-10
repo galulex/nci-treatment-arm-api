5c93142c3cd6bc9f6da5cfc12a8a267760d4593f
asampatoor <adithya.sampatoor@nih.gov>
2017-01-10 12:01:35 -0500

    Adding in Ability model

---
+++ app/models/ability.rb
@@ -0,0 +1,22 @@
+class Ability
+  include CanCan::Ability
+
+  attr_reader :methods
+
+  def initialize(user = {})
+    user.deep_symbolize_keys!
+    user = user.dig(:roles) || []
+    user.each do | role |
+      begin
+        methods << "NciMatchRoles::#{role.downcase.classify}".constantize.get_methods
+      rescue NameError
+        methods = []
+      end
+    end
+    can methods, :all
+  end
+
+  def methods
+    @methods ||= []
+  end
+end
