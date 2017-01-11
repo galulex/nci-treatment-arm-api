3b08b08a40a2df18e471a95170112b96458b6edb
asampatoor <adithya.sampatoor@nih.gov>
2017-01-11 09:57:28 -0500

    Adding in Abilitiy specs

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,20 @@
+describe Ability, type: :model do
+
+  it 'can create empty object' do
+    expect(Ability.new()).to_not be nil
+  end
+
+  describe 'should handle roles correctly' do
+    it 'should not allow management when blank' do
+      expect(Ability.new().can?(:manage, :all)).to eq(false)
+    end
+
+    # it "allow admin to manage all" do
+    #   expect(Ability.new({:roles => ["ADMIN"]}).can?(:manage, :all)).to eq(true)
+    # end
+
+    it 'should allow System to manage all' do
+      expect(Ability.new({ roles: ["SYSTEM"] }).can?(:manage, :all)).to eq(true)
+    end
+  end
+end
