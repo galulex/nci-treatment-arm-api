c19d6563cfa963bfb42de586797083f8f16316c8
asampatoor <adithya.sampatoor@nih.gov>
2017-01-11 13:50:04 -0500

    Adding in Exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   rescue_from Seahorse::Client::NetworkingError, with: :no_db_connection_exception
-  rescue_from ActionController::RoutingError, with: -> (exception) { render_error(:bad_request, exception) }
-  rescue_from CanCan::AccessDenied, with: -> (exception) { render_error_with_message(:unauthorized, exception) }
+  rescue_from ActionController::RoutingError, with: ->(exception) { render_error(:bad_request, exception) }
+  rescue_from CanCan::AccessDenied, with: ->(exception) { render_error_with_message(:unauthorized, exception) }
+  rescue_from NameError, with: ->(exception) { render_error(:internal_server_error, exception) }
 
   protected
 
