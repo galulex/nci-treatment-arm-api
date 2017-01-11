04ad0a683d394434f09d15d93dd42ece2e8c2ef3
asampatoor <adithya.sampatoor@nih.gov>
2017-01-11 10:45:13 -0500

    Refactoring Exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-  rescue_from Aws::DynamoDB::Errors::ValidationException, with: :resource_not_found_exception
   rescue_from Seahorse::Client::NetworkingError, with: :no_db_connection_exception
   rescue_from ActionController::RoutingError, with: -> (exception) { render_error(:bad_request, exception) }
-  rescue_from CanCan::AccessDenied, with: -> (exception) { render_error(:unauthorized, exception) }
+  rescue_from CanCan::AccessDenied, with: -> (exception) { render_error_with_message(:unauthorized, exception) }
 
   protected
 
   def render_error(status, exception)
     logger.error status.to_s +  " " + exception.to_s
-    respond_to do | format |
-      format.all { render json: { error: exception.to_s }, status: status }
+    respond_to do |format|
+      format.all { head status, message: exception.message }
     end
   end
 
-  def resource_not_found_exception
-    render json: { message: "One of the Tables doesn't exist, Please create the table by restarting the TreatmentArmProcessorAPI server" }, status: 400
+  def render_error_with_message(status, exception)
+    logger.error status.to_s +  " " + exception.to_s
+    render json: { message: exception.message }, status: status
   end
 
   def no_db_connection_exception
