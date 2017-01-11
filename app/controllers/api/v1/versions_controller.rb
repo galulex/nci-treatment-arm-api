e367144efda1b9312acc7f9377bcdbe77350ff99
asampatoor <adithya.sampatoor@nih.gov>
2017-01-11 10:51:30 -0500

    Making the code DRY

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -26,13 +26,6 @@ module Api
           standard_error_message(error)
         end
       end
-
-      private
-
-      def standard_error_message(error)
-        logger.error error.message
-        render json: error.to_json, status: 500
-      end
     end
   end
 end
