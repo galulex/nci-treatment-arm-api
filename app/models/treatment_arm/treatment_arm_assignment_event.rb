9eee201d94e8bad0139d38c84db17e674f05c0ff
asampatoor <adithya.sampatoor@nih.gov>
2017-02-03 14:57:09 -0500

    display the patient_status_reason as the assignment_reason at some patient statuses

--- app/models/treatment_arm/treatment_arm_assignment_event.rb
+++ app/models/treatment_arm/treatment_arm_assignment_event.rb
@@ -68,6 +68,13 @@ class TreatmentArmAssignmentEvent
       hash_merge(assay_stats, assignment.matched_treament_arm_for_assay_rules('assay_rules'))
       hash_merge(assignment_assay_stats, assignment.matched_treament_arm_for_assay_rules('assignment_assay_rules'))
     end
+
+    treatment_arm_assignments.each do |assignment|
+      if %w(PREVIOUSLY_ON_ARM PREVIOUSLY_ON_ARM_OFF_STUDY NOT_ENROLLED_ON_ARM NOT_ENROLLED_ON_ARM_OFF_STUDY).include?(assignment.patient_status)
+        assignment.assignment_reason = assignment.patient_status_reason
+      end
+    end
+
     if treatment_arm_assignments.present?
       {
         patients_list: treatment_arm_assignments.collect(&:to_h),
