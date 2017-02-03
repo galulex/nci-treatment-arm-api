66ee8723b1129b11013e62cef69b3f901d1f957a
asampatoor <adithya.sampatoor@nih.gov>
2017-02-03 15:23:30 -0500

    Removing unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   rescue_from Seahorse::Client::NetworkingError, with: :no_db_connection_exception
-  rescue_from ActionController::RoutingError, with: ->(exception) { render_error(:bad_request, exception) }
   rescue_from CanCan::AccessDenied, with: ->(exception) { render_error_with_message(:unauthorized, exception) }
   rescue_from NameError, with: ->(exception) { render_error(:internal_server_error, exception) }
 
   protected
-
-  def render_error(status, exception)
-    logger.error status.to_s +  " " + exception.to_s
-    respond_to do |format|
-      format.all { head status, message: exception.message }
-    end
-  end
 
   def render_error_with_message(status, exception)
     logger.error status.to_s +  " " + exception.to_s
