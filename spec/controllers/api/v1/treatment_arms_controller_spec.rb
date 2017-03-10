445a59b7cf896a8316acddd0bb8ec10e7cb92428
asampatoor <adithya.sampatoor@nih.gov>
2017-03-10 16:51:39 -0500

    Moving the MockcogService unit tests to the Controllers

--- spec/controllers/api/v1/treatment_arms_controller_spec.rb
+++ spec/controllers/api/v1/treatment_arms_controller_spec.rb
@@ -301,4 +301,22 @@ describe Api::V1::TreatmentArmsController do
       allow(TreatmentArm).to receive(:scan).and_return([])
     end
   end
+
+  describe 'MockCogService' do
+    it "Should call Mock COG when the actual COG is down or wan't able to connect" do
+      allow(HTTParty::Request).to receive(:new).and_return(HTTParty::Request)
+      allow(HTTParty::Response).to receive(:new).and_return(HTTParty::Response)
+      allow(HTTParty::Request).to receive(:perform).and_return(HTTParty::Response)
+      allow(HTTParty::Response).to receive(:code).and_return(200)
+      expect(MockCogService.perform).to be_truthy
+    end
+
+    it 'should raise exception when unable to connect to Mock COG' do
+      allow(HTTParty::Request).to receive(:new).and_return(HTTParty::Request)
+      allow(HTTParty::Response).to receive(:new).and_return(HTTParty::Response)
+      allow(HTTParty::Request).to receive(:perform).and_return(HTTParty::Response)
+      allow(HTTParty::Response).to receive(:code).and_return(500)
+      allow(MockCogService.perform).to receive(:scan).and_raise('this error')
+    end
+  end
 end
