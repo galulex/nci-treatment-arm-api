542a0777fc9314905e578ba67874ba1950e261f5
asampatoor <adithya.sampatoor@nih.gov>
2017-03-22 13:48:07 -0400

    rendering version call in json

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -8,20 +8,15 @@ module Api::V1
           @hash = {}
           document.each_line do |line|
             arr = line.split('=', 2)
-            @hash.store(arr[0], arr[1])
+            @hash.store(arr[0], arr[1].squish!)
           end
           document.close
         end
-        @version = TreatmentArmApi::Application.version
-        @rails_version = Rails::VERSION::STRING
-        @ruby_version = RUBY_VERSION
-        @running_on = @hash['Commit'].present? ? @hash['Commit'] : ''
-        @author = @hash['Author'].present? ? @hash['Author'] : ''
-        @travisbuild = @hash['TravisBuild'].present? ? @hash['TravisBuild'] : ''
-        @travisjob = @hash['TravisBuildID'].present? ? @hash['TravisBuildID'] : ''
-        @dockerinstance = @hash['Docker'].present? ? @hash['Docker'] : ''
-        @buildtime = @hash['BuildTime'].present? ? @hash['BuildTime'] : ''
-        @environment = Rails.env
+        @hash[:version] = TreatmentArmApi::Application.version
+        @hash[:rails_version] = Rails::VERSION::STRING
+        @hash[:ruby_version] = RUBY_VERSION
+        @hash[:environment] = Rails.env
+        render json: @hash
       rescue => error
         standard_error_message(error)
       end
